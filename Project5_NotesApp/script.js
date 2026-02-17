ad27abec05868a5769331a6a469f242cec76b9ce
Atharv <atharvgupta098@gmail.com>
2026-02-18 00:28:43 +0530

    Added local browser storage

--- Project5_NotesApp/script.js
+++ Project5_NotesApp/script.js
@@ -1,11 +1,16 @@
 const createBtn = document.getElementById("add-btn");
-const notesContainer = document.getElementsByClassName("notes_container")[0];
+const notesContainer = document.querySelector(".notes_container");
 let notes = document.querySelectorAll(".input-box");
 
+function saveData(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+function showTask(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showTask();
 
 function AddNotes() {
-    console.log("function call");
-    
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
@@ -13,12 +18,27 @@ function AddNotes() {
     img.className = "delete-btn";
     img.src = "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
+    saveData();
 }
-// AddNotes();
 createBtn.addEventListener("click", AddNotes);
 
 notesContainer.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove()
+        saveData();
+    }else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach( nt => {
+            nt.onkeyup = function(){
+                saveData();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown" , event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
